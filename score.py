48896b54824b6cab65a4cc116be97c5b8f4f54e3
Otniel van Handel <otnivh@gmail.com>
2020-10-10 15:52:40 +0300

    update fields to match new API & remove apikey from redis key (#2)

--- score.py
+++ score.py
@@ -7,11 +7,9 @@ from operator import itemgetter
 from joblib import Parallel, delayed
 from collections import namedtuple
 
-
 THREADS = 200
 
-
-ScoreEntry = namedtuple('Score', ['ticker', 'grossGrowth', 'incomeGrowth', 'RnDRatio', 'cashPerDebt',
+ScoreEntry = namedtuple('Score', ['ticker', 'grossProfitGrowth', 'incomeGrowth', 'RnDRatio', 'cashPerDebt',
                                   'netIncome', 'mktCap'])
 
 
@@ -40,7 +38,8 @@ class BaseScore:
         :return: List where each row contains the ticker name and a scores (maybe more than 1)
         """
 
-        score_list: [ScoreEntry] = Parallel(n_jobs=THREADS, prefer="threads")(delayed(self.process_ticker)(ticker) for ticker in self.ticker_list)
+        score_list: [ScoreEntry] = Parallel(n_jobs=THREADS, prefer="threads")(
+            delayed(self.process_ticker)(ticker) for ticker in self.ticker_list)
 
         self.score_list: [ScoreEntry] = [score for score in score_list if score]
         self.filter()
@@ -58,7 +57,7 @@ class BaseScore:
     def process_ticker(self, ticker) -> ScoreEntry:
         financial_dict = {}
         try:
-            for statement_type in [Statements.Income, Statements.BalanceSheet, Statements.CashFlow]:
+            for statement_type in [Statements.Income, Statements.BalanceSheet]:
                 data_by_year = data.get_financials(ticker, statement=statement_type, period=Period.Year)
                 data_until_today = self.filter_by_date(data_by_year)
                 financial_dict[statement_type] = data_until_today
@@ -67,7 +66,7 @@ class BaseScore:
                 profile=data.get_financials(ticker, statement=Statements.Profile, period=Period.Year),
                 income_list=financial_dict[Statements.Income],
                 balance_sheet_list=financial_dict[Statements.BalanceSheet],
-                cash_flow_list=financial_dict[Statements.CashFlow]
+                # cash_flow_list=financial_dict[Statements.CashFlow]
             )
             return self.score(ticker, ticker_data)
         except Exception as e:
@@ -85,37 +84,31 @@ class ScoreExample(BaseScore):
         :param ticker:
         :return: a tuple
         """
-        growth_score = 0.0
         rnd_score = 0.0
         debt_score = 0
-        net_income_score = 0
-        income_growth_score = 0
 
         income_list = ticker_data.income_list
         balance_sheet_list = ticker_data.balance_sheet_list
         for i in range(len(income_list)):
+
             try:
-                growth_score += (float(income_list[i + 1].GrossProfit) - float(income_list[i].GrossProfit)) / float(
-                    income_list[i].GrossProfit)
-                debt_score += float(balance_sheet_list[i].TotalAssets) / float(balance_sheet_list[i].TotalLiabilities)
+                debt_score += float(balance_sheet_list[i].TotalAssets) / max(
+                    float(balance_sheet_list[i].TotalLiabilities),
+                    float(balance_sheet_list[i].TotalAssets) / 5)  # handle case of zero or very low debt for one year
                 rnd_score += float(income_list[i].RnDExpenses) / float(income_list[i].OperatingExpenses)
-                net_income_score += float(income_list[i].NetIncome)
-                income_growth_score += (float(income_list[i + 1].NetIncome) - float(income_list[i].NetIncome)) / float(
-                    income_list[i].NetIncome)
+
             except IndexError:
-                pass
+                print(f'index {i!r} out of bounds for {ticker!r}')
             except ZeroDivisionError:
                 print(f'{ticker!r} revenue is zero for {income_list[i].Date}')
-            # except ValueError:
-            #     pass
 
         return ScoreEntry(
             ticker=ticker,
-            grossGrowth=growth_score / max(len(income_list) - 1, 1),
-            incomeGrowth=income_growth_score / max(len(income_list) - 1, 1),
+            grossProfitGrowth=avg_growth(ticker, income_list, 'GrossProfit'),
+            incomeGrowth=avg_growth(ticker, income_list, 'NetIncome'),
             RnDRatio=rnd_score / max(len(income_list), 1),
             cashPerDebt=debt_score / max(len(income_list), 1),
-            netIncome=net_income_score / max(len(income_list), 1),
+            netIncome=average(ticker, income_list, 'NetIncome'),
             mktCap=ticker_data.profile.mktCap
         )
 
@@ -125,10 +118,33 @@ class ScoreExample(BaseScore):
     def filter(self):
         filtered_list: [ScoreEntry] = []
         for score in self.score_list:
-            if (0.2 < score.grossGrowth < 0.5
-                    and 0.2 < score.RnDRatio < 0.7
-                    and 0.41 < score.cashPerDebt
-                    and 0 < score.netIncome):
+            if (1.25 < score.grossProfitGrowth < 1.5
+                    and 0.25 < score.RnDRatio < 0.7):
+                # and 0.2 < score.RnDRatio < 0.7
+                # and 0.41 < score.cashPerDebt
+                # and 0 < score.netIncome
                 filtered_list.append(score)
         self.score_list = filtered_list
 
+
+def avg_growth(ticker: str, my_list: List, field: str) -> float:
+    acc_growth = 1
+    for i in range(len(my_list) - 1):
+        start_data = my_list[i]
+        end_data = my_list[i+1]
+
+        start = float(start_data._asdict().get(field))
+        end = float(end_data._asdict().get(field))
+        if end != 0:
+            acc_growth += (end - start) / start
+        else:
+            print(f'ticker {ticker!r} {field} is zero for {end_data.Date}')
+            acc_growth += 1
+    return acc_growth / max(len(my_list) - 1.0, 1.0)
+
+
+def average(ticker: str, my_list: List, field: str) -> float:
+    my_sum: float = 0
+    for i in range(len(my_list)):
+        my_sum += float(my_list[i]._asdict().get(field))
+    return my_sum / max(len(my_list), 1)
