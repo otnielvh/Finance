43bc9be66d317426c3c457f5d1895780bf8a3d09
ohad-israeli <ohadisra@gmail.com>
2021-01-01 10:59:40 +0200

    add ticker price

--- data/ticker_price.py
+++
@@ -1,9 +0,0 @@
-import yfinance as yf
-from pandas.core.frame import DataFrame
-
-
-def get_price(ticker: str) -> DataFrame:
-    yf_ticker = yf.Ticker(ticker)
-    # allowed periods are: 1d,5d,1mo,3mo,6mo,1y,2y,5y,10y,ytd,max
-    price_history = yf_ticker.history(period="max")
-    return price_history
