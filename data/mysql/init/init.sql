391c1fc9185c6e0cbeb5bf7cd817eb748118fd1e
ohad-israeli <ohadisra@gmail.com>
2021-01-16 09:19:18 +0200

    mysql support

---
+++ data/mysql/init/init.sql
@@ -0,0 +1,11 @@
+CREATE USER 'finuser'@'%' IDENTIFIED WITH mysql_native_password BY 'fin';
+GRANT ALL PRIVILEGES ON * . * TO 'finuser'@'%';
+
+CREATE TABLE IF NOT EXISTS sec_idx (
+    cik INT,
+    year INT,
+    company TEXT,
+    report_type TEXT,
+    url TEXT,
+    PRIMARY KEY (sec, year)
+);
