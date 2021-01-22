7b4d826104d0c25753eec5aeee10a4c08c202f27
otnielvh <otniel@redislabs.com>
2021-01-22 23:15:46 +0200

    flask

--- common/config.py
+++ common/config.py
@@ -1,8 +1,10 @@
-REDIS_HOST_NAME = 'localhost'
+import os
+
+REDIS_HOST_NAME = os.getenv('REDIS_URL') or 'localhost'
 REDIS_PORT = 6379
 ASSETS_DIR = './assets/'
 
-DB_HOST_NAME = 'localhost'
+DB_HOST_NAME = os.getenv('MYSQL_URL') or 'localhost'
 DB_USER = 'finuser'
 DB_PASSWORD = 'fin'
 DB_NAME = 'findb'
