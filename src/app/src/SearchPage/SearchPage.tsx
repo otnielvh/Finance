483a9462bc9d264426345fd7cca50c87e796bff8
Otniel van Handel <otnivh@gmail.com>
2021-02-14 07:30:24 +0200

    support multi query paramters in the UI (#48)

--- src/app/src/SearchPage/SearchPage.tsx
+++ src/app/src/SearchPage/SearchPage.tsx
@@ -1,15 +1,16 @@
 import React, {useEffect} from 'react';
 import axios from "axios";
 
-import Select from '@material-ui/core/Select';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
-import FormControl from '@material-ui/core/FormControl';
-import InputLabel from '@material-ui/core/InputLabel';
 import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
+
+
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 
 import TickerScoreList from "./TickerList";
+import QueryFilter from "./QueryFilter";
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
@@ -20,19 +21,42 @@ const useStyles = makeStyles((theme: Theme) =>
         root: {
             flexGrow: 1,
         },
+        margin: {
+            margin: theme.spacing(1),
+        },
     }),
 );
 
 export default function SearchPage () {
-    const [filterList, setFilterList] = React.useState([]);
     const classes = useStyles();
-    const gridSize = 3;
 
+
+    const [filterList, setFilterList] = React.useState([]);
     const [isMounted, setIsMounted] = React.useState(false);
     const [tickerScoreList, setTickerScoreList ] = React.useState([]);
-    const [chosenFilter, setChosenFilter] = React.useState("");
-    const [queryMin, setQueryMin] = React.useState(0);
-    const [queryMax, setQueryMax] = React.useState(100);
+
+    const [queryCount, setQueryCount] = React.useState(1);
+    const [chosenFilter, setChosenFilter] = React.useState([""]);
+    const [queryMin, setQueryMin] = React.useState([0]);
+    const [queryMax, setQueryMax] = React.useState([100]);
+
+    const setIndexedChosenFilter = (index: number, value: string) => {
+        const myChosenFilter = [...chosenFilter];
+        myChosenFilter[index] = value;
+        setChosenFilter(myChosenFilter);
+    }
+
+    const setIndexedQueryMax = (index: number, value: number) => {
+        const myQueryMax = [...queryMax];
+        myQueryMax[index] = value;
+        setQueryMax(myQueryMax);
+    }
+
+    const setIndexedQueryMin = (index: number, value: number) => {
+        const myQueryMin = [...queryMin];
+        myQueryMin[index] = value;
+        setQueryMin(myQueryMin);
+    }
 
     useEffect( () => {
             if (!isMounted){
@@ -41,7 +65,7 @@ export default function SearchPage () {
                     .then((res) => {
                         const filterList = res.data.filters;
                         setFilterList(filterList);
-                        setChosenFilter(filterList[0]);
+                        setChosenFilter([filterList[0]]);
                     })
                 setIsMounted(true);
                 axios.get(`/ticker-scores`, {"params": {"short_list": "true"}})
@@ -53,82 +77,73 @@ export default function SearchPage () {
         [isMounted]
     )
 
-    const handleSelectFilter = (event: React.ChangeEvent<{ name?: string | undefined; value: unknown }>) => {
+    const handleIndexedChosenFilter = (index: number, event: React.ChangeEvent<{ name?: string | undefined; value: unknown }>) => {
         const chosenFilter = event.target.value as string;
-        setChosenFilter(chosenFilter);
+        setIndexedChosenFilter(index, chosenFilter);
     };
 
     const handleSearchBtn = () => {
+        let _filterList = []
+        for (let i=0; i < queryCount; i++) {
+            _filterList.push({
+                name: chosenFilter[i],
+                min: queryMin[i],
+                max: queryMax[i],
+            })
+        }
         axios.post(`/ticker-scores`,
-            {"filters": [
-                    {name: chosenFilter, min: queryMin, max: queryMax}
-                ]},
+            {"filters": _filterList},
             {"params": {"short_list": "true"}},
         ).then((res) => {
             setTickerScoreList(res.data.score_list);
         })
     }
 
+
+    const queryFilterList = []
+    for (let i = 0; i < queryCount; i++) {
+        queryFilterList.push(
+            <QueryFilter
+                index={i}
+                chosenFilter={chosenFilter[i]}
+                handleSelectFilter={handleIndexedChosenFilter}
+                filterList={filterList}
+                queryMax={queryMax[i]}
+                setQueryMin={setIndexedQueryMin}
+                queryMin={queryMin[i]}
+                setQueryMax={setIndexedQueryMax}
+            />
+        )
+    }
+
     return(
         <div>
             <Grid container justify="center" className={classes.root}>
-                <Grid container justify="center">
-
-                    <Grid item xs={gridSize}>
-                        <FormControl className={classes.formControl}>
-                            <TextField
-                                label="Minimum"
-                                type="number"
-                                value={queryMin}
-                                onChange={(event) =>
-                                    setQueryMin(parseFloat(event.target.value))}
-                                InputLabelProps={{ shrink: true }}
-                                variant="outlined"
-                            />
-                        </FormControl>
-                    </Grid>
-
-                    <Grid item xs={gridSize}>
-                        <FormControl className={classes.formControl}>
-                            <InputLabel shrink>Algo Queries</InputLabel>
-                            <Select
-                                native
-                                value={chosenFilter}
-                                onChange={handleSelectFilter}
-                                inputProps={{
-                                    name: chosenFilter,
-                                }}
-                            >
-                                {filterList.map(filter => <option key={filter} value={filter}>{filter}</option>)}
-                            </Select>
-                        </FormControl>
-                    </Grid>
-
-                    <Grid item xs={gridSize}>
-                        <FormControl className={classes.formControl}>
-                            <TextField
-                                id="maximum"
-                                label="Maximum"
-                                type="number"
-                                value={queryMax}
-                                onChange={(event) =>
-                                    setQueryMax(parseFloat(event.target.value))}
-                                InputLabelProps={{
-                                    shrink: true,
-                                }}
-                                variant="outlined"
-                            />
-                        </FormControl>
-                    </Grid>
+                <Grid container>
+                    {queryFilterList}
                 </Grid>
-                <Grid item xs={12} justify="center">
+
+
+                <Grid item xs={3} justify="flex-start">
                     <Button onClick={handleSearchBtn} variant="contained" color="primary">
                         Search
                     </Button>
                 </Grid>
+                <Grid item xs={3} justify="flex-end">
+                    <Fab size="small" color="primary" aria-label="add" className={classes.margin}>
+                        <AddIcon onClick={() => {
+                            setChosenFilter([...chosenFilter, filterList[0]])
+                            setQueryMax([...queryMax, 100])
+                            setQueryMin([...queryMin, 0])
+                            setQueryCount(queryCount + 1)
+                        }}/>
+                    </Fab>
+                </Grid>
+
                 <Grid justify="center">
                     <TickerScoreList filterList={filterList} tickerScoreList={tickerScoreList}/>
                 </Grid>
+
             </Grid>
 
         </div>
